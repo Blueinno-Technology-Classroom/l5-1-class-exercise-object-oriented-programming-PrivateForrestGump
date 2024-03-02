b391469f91216c48527e1cc9640a537da93cefa7
PrivateForrestGump <forrestleung58@gmail.com>
2024-03-02 15:01:32 +0800

    dd

--- main.py
+++ main.py
@@ -2,23 +2,42 @@
 '''
 Q1a: 
 '''
-
+x = 9
+y = 2
 ##################################################
 '''
 Q1b: 
 '''
-
+x = 27
+y = 2
 ##################################################
 '''
 Q2:
 '''
-
+class Name:
+    def __init__(self, first_name, last_name):
+        self.fn = first_name
+        self.ln = last_name
+    
 
 ##################################################
 '''
 Q3:
 '''
+class Name:
+    def __init__(self, first_name, last_name):
+        self.fn = first_name
+        self.ln = last_name
 
+    def normal_order(self):
+        print(f'{self.fn} {self.ln}')
+
+    def reverse_order(self):
+        print(f'{self.ln}, {self.fn}')
+
+killer = Name('John', 'Wick')
+killer.normal_order()
+killer.reverse_order()
 
 ##################################################
 '''
